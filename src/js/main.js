a178f34b558fa5eeff4a7bcddfa6d109867a9634
Gyuri-v <fnffkdll@naver.com>
2022-09-19 16:49:40 +0900

    camera, look point 와 route 생성 완료, scroll 애니메이션 적용 / point 찍기가 조금 남음

--- src/js/main.js
+++ src/js/main.js
@@ -6,17 +6,53 @@ import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
+const container = document.querySelector('#container');
+
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
 
 const clock = new THREE.Clock();
-let renderer, scene, camera, orbitControls, dragControls, mixer, gui;
+
+let renderer, scene, camera, orbitControls, cameraDragControls, lookDragControls, mixer, gui;
+let cameraPoints = [];
 let lookPoints = [];
+let cameraMoves;
+let cameraMovesPoints = [];
+let lookMoves;
+let lookMovesPoints = [];
+let lookPointFirst;
+let currentCamera;
+let currentLook;
+
+let chkOrbitControl = false;
+let chkDebug = true;
 
 const value = {
+    camera: [
+        new THREE.Vector3(-2.2,   0,    -3.5),
+        new THREE.Vector3( 1.2,   0.5,  -6.3),
+        new THREE.Vector3( 6,     0.51, -5.92),
+        new THREE.Vector3( 3.73,  2.03, -7.24),
+        new THREE.Vector3( 7.8,   2,    -2.8 ),
+        new THREE.Vector3( 4.5,   0,     0 ),
+        // new THREE.Vector3( 4,     0,     0 ),
+        new THREE.Vector3( 3.09, -0.69, -0.19 ),
+        // new THREE.Vector3( 2.0,  -0.7,  -0.2 ),
+        new THREE.Vector3( 1.18, -0.6, -0.14 ),
+        new THREE.Vector3( 0.74, -0.42,  0.32 ),
+    ],
     look: [
-        new THREE.Vector3(-2.4, -0.5, 0),
-        new THREE.Vector3(-1.4, -0.5, -1.8),
+        new THREE.Vector3(-2.2,  -0.5,   0),
+        new THREE.Vector3(-1.4,  -0.5,  -1.8),
+        // new THREE.Vector3( 0.3,  -0.3,  -2.7),
+        new THREE.Vector3( 2.5,  -0.3,  -2.7),
+        new THREE.Vector3( 2.67,  0.53, -2.11),
+        new THREE.Vector3( 2.8,   0.3,  -0.2),
+        // new THREE.Vector3( 2.6,  -0.5,  -0.2),
+        new THREE.Vector3( 1.8,  -0.8,  -0.2),
+        new THREE.Vector3( 0.75,  0.4, -0.15),
+        new THREE.Vector3( 0.63,  0.7,  0.04),
+        new THREE.Vector3( 0.6,  -0.5,   1.12),
     ],
 }
 
@@ -26,7 +62,7 @@ const init = function () {
     renderer.setSize(WIDTH, HEIGHT);
     renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
     renderer.outputEncoding = THREE.sRGBEncoding;
-    document.querySelector('#container').appendChild( renderer.domElement );
+    container.appendChild( renderer.domElement );
 
     // PmremGenerator
     const pmremGenerator = new THREE.PMREMGenerator( renderer );
@@ -41,15 +77,6 @@ const init = function () {
     camera.position.set(5, 3, 8);
     camera.lookAt(0, 0.5, 0);
     scene.add(camera);
-
-    // Helper
-    gui = new dat.GUI();
-    gui.add(camera.position, 'x', 0, 20, 0.01).name('camera p x');
-    gui.add(camera.position, 'y', 0, 20, 0.01).name('camera p y');
-    gui.add(camera.position, 'z', 0, 20, 0.01).name('camera p z');
-    gui.add(camera.rotation, 'x', -3, 3, 0.001).name('camera r x');
-    gui.add(camera.rotation, 'y', -3, 3, 0.001).name('camera r y');
-    gui.add(camera.rotation, 'z', -3, 3, 0.001).name('camera r z');
 
     // Controls
     orbitControls = new OrbitControls( camera, renderer.domElement );
@@ -76,48 +103,109 @@ const init = function () {
         console.error( e );
     } 
 
-    createPoint();
+    createRoute();
+}
+
+const scroll = function () {
+    let scrollTop = window.pageYOffset;
+    let moveArea = container.offsetHeight - window.innerHeight;
+    let percent = scrollTop / moveArea;
+
+    currentCamera = Math.round(percent * (cameraMovesPoints.length - 1));
+    currentLook = Math.round(percent * (lookMovesPoints.length - 1));
 }
 
 const draw = function () {
     const delta = clock.getDelta();
 
-    orbitControls.update();
-    if( mixer ) mixer.update( delta );
+    if ( chkOrbitControl ) {
+        orbitControls.update();
+    } else {
+        orbitControls.enabled = false;
+    
+        if ( cameraMovesPoints[currentCamera] ) {
+            camera.position.lerp(cameraMovesPoints[currentCamera], 0.05);
+            lookPointFirst.lerp(lookMovesPoints[currentLook], 0.05);
+            camera.lookAt(lookPointFirst);
+        }
+    }
+    if ( mixer ) mixer.update( delta );
 
     renderer.render( scene, camera );
     renderer.setAnimationLoop(draw);
 }
 
-const createPoint = function () {
-    const pointGeometry = new THREE.SphereGeometry(0.5);
-    const pointMaterial = new THREE.MeshBasicMaterial({ color: 'blue' });
-    
-    // camera
-
-    // look
-    const lookPoint = new THREE.Mesh(pointGeometry, pointMaterial);
-    lookPoint.position.set(-2.4, -0.5, 0);
-    scene.add(lookPoint);
-    lookPoints.push(lookPoint);
-
-    gui.add(lookPoint.position, 'x', -50, 100, 0.01).name('lookPoint x');
-    gui.add(lookPoint.position, 'y', -50, 100, 0.01).name('lookPoint y');
-    gui.add(lookPoint.position, 'z', -50, 100, 0.01).name('lookPoint z');
-
-
-    
-    dragControls = new DragControls( lookPoint, camera, renderer.domElement );
-    console.log(dragControls.domElement);
-    // dragControls.domElement.addEventListener('click', function () {
-    //     dragControls.lock();
-    // })
-    
-}
-
 const createRoute = function () {
-    //
+    const pointGeometry = new THREE.SphereGeometry(0.05);
+    const pointMaterialBlue = new THREE.MeshBasicMaterial({ color: 'blue' });
+    const pointMaterialRed = new THREE.MeshBasicMaterial({ color: 'red' });
+    
+    // camera - point
+    for (let i = 0; i < value.camera.length; i++) {
+        const cameraPoint = new THREE.Mesh(pointGeometry, pointMaterialRed);
+        cameraPoint.position.set(value.camera[i].x, value.camera[i].y, value.camera[i].z);
+        cameraPoint.name = `lookPoint_${i}`;
+        cameraPoints.push(cameraPoint);
+
+        if ( chkDebug ) scene.add(cameraPoint);
+    }
+    if ( chkDebug ) {
+        cameraDragControls = new DragControls( cameraPoints, camera, renderer.domElement );
+        cameraDragControls.addEventListener('dragstart', e => {
+            orbitControls.enabled = false;
+        });
+        cameraDragControls.addEventListener('dragend', e => {
+            console.log(e.object.position);
+            orbitControls.enabled = true;
+        });
+    }
+
+    // camera - line
+    cameraMoves = new THREE.CatmullRomCurve3( value.camera );
+    cameraMovesPoints = cameraMoves.getSpacedPoints(200);
+    const cameraMoveLine = new THREE.Line(
+        new THREE.BufferGeometry().setFromPoints(cameraMovesPoints),
+        new THREE.LineBasicMaterial({ color: 'orange' })
+    );
+    if ( chkDebug ) scene.add(cameraMoveLine);
+
+    // look - point
+    for (let i = 0; i < value.look.length; i++) {
+        const lookPoint = new THREE.Mesh(pointGeometry, pointMaterialBlue);
+        lookPoint.position.set(value.look[i].x, value.look[i].y, value.look[i].z);
+        lookPoint.name = `lookPoint_${i}`;
+        if ( chkDebug ) scene.add(lookPoint);
+        lookPoints.push(lookPoint);
+    }
+    if ( chkDebug ) {
+        lookDragControls = new DragControls( lookPoints, camera, renderer.domElement );
+        lookDragControls.addEventListener('dragstart', e => {
+            orbitControls.enabled = false;
+        });
+        lookDragControls.addEventListener('dragend', e => {
+            console.log(e.object.position);
+            orbitControls.enabled = true;
+        });
+    }
+
+    // look - line
+    lookPointFirst = value.look[0].clone();
+    lookMoves = new THREE.CatmullRomCurve3( value.look );
+    lookMovesPoints = lookMoves.getSpacedPoints(200);
+    const lookMoveLine = new THREE.Line(
+        new THREE.BufferGeometry().setFromPoints(lookMovesPoints),
+        new THREE.LineBasicMaterial({ color: 'purple' })
+    );
+    if ( chkDebug ) scene.add(lookMoveLine)
 }
 
 init();
 draw();
+scroll();
+window.addEventListener('scroll', scroll);
+
+// if ( chkOrbitControl ) {
+//     window.addEventListener('click', function() {
+//         console.log('camera position', camera.position );
+//     })
+// } 
